4ede01fac01447e7bda865a52f0b6ea9a9c16f53
Ryo Akimoto <autumn.1006.0510@gmail.com>
2022-11-03 01:47:20 +0900

    modified config.sample.sh

--- config.sample.sh
+++ config.sample.sh
@@ -3,6 +3,7 @@
 readonly COMPANY_SERVER_URL='https://[この部分]/'
 readonly EMAIL='アカウントのメールアドレス'
 readonly API_TOKEN='API トークン（作成の手順は README.md に記載）'
+readonly SPACE_KEY=''
 readonly X_ROOT_PAGE_ID='テンプレートを格納しているページの親ページID(暫定)'
 readonly X_TEMPLATE_PAGE_ID='テンプレートを格納しているページID'
 readonly THIRD_ROOT_PAGE_ID='テンプレートを格納しているページの親ページID(暫定)'

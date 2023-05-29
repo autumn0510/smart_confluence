52986bed60e4cf920b924afe5da11e6dfb658881
autumn0510 <autumn.1006.0510@gmail.com>
2023-05-30 07:42:57 +0900

    updated

--- config.sample.sh
+++ config.sample.sh
@@ -6,5 +6,3 @@ readonly API_TOKEN='API トークン（作成の手順は README.md に記載）
 readonly SPACE_KEY=''
 readonly X_ROOT_PAGE_ID='テンプレートを格納しているページの親ページID(暫定)'
 readonly X_TEMPLATE_PAGE_ID='テンプレートを格納しているページID'
-readonly THIRD_ROOT_PAGE_ID='テンプレートを格納しているページの親ページID(暫定)'
-readonly THIRD_TEMPLATE_PAGE_ID='テンプレートを格納しているページID'
